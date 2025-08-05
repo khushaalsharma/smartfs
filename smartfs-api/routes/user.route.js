d8f6acbceb776679439c4505e9a360579fb9bd44
Khushaal Sharma <khushaal@veersatech.com>
2025-08-06 01:26:40 +0530

    removed the user Manager for SQL atble

--- smartfs-api/routes/user.route.js
+++ smartfs-api/routes/user.route.js
@@ -7,7 +7,6 @@ import jwt from "jsonwebtoken";
 import { configDotenv } from "dotenv";
 
 //managers
-import userManager from "../managers/userManager.js";
 
 
 configDotenv();
@@ -33,25 +32,5 @@ router.use(bodyParser.urlencoded(
 router.use(cookieParser());
 
 
-router.post("/newUser", async(req, res) => {
-    res.header("Access-Control-Allow-Methods", "POST");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-
-    const {firebaseId} = req.body;
-    try{
-        const user = new userManager();
-
-        let result = await user.createNewUser(firebaseId);
-
-        if(result != null){
-            return res.status(200).json(result);
-        }
-
-        return res.status(400).json("error in creating user: " + result);
-    }
-    catch(error){
-        return res.status(500).json(error);
-    }
-});
 
 export default router;
