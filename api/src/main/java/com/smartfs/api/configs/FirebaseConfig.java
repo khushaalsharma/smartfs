f9cdca6577c50f62afd737c536bb04e2693e9f7d
khushaalsharma <khushaalsharma1@gmail.com>
2026-03-08 18:58:16 +0530

    docker changes

--- api/src/main/java/com/smartfs/api/configs/FirebaseConfig.java
+++ api/src/main/java/com/smartfs/api/configs/FirebaseConfig.java
@@ -4,10 +4,11 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -16,7 +17,9 @@ public class FirebaseConfig {
     public void init() throws IOException {
         try {
             if(FirebaseApp.getApps().isEmpty()){
-                FileInputStream firebaseAccessKey = new FileInputStream("src/main/resources/firebaseSecKey.json");
+                //FileInputStream firebaseAccessKey = new FileInputStream("src/main/resources/firebaseSecKey.json");
+                ClassPathResource resource = new ClassPathResource("firebaseSecKey.json");
+                InputStream firebaseAccessKey = resource.getInputStream();
 
                 FirebaseOptions options = FirebaseOptions.builder()
                         .setCredentials(GoogleCredentials.fromStream(firebaseAccessKey))
